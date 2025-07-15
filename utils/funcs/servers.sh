e8bc26852d46c75fad0c2234e34fd37b4ee292e1
Theron Steinbrecher <theron.stein@gmail.com>
2025-07-15 16:10:34 -0500

    trying...

--- utils/funcs/servers.sh
+++ utils/funcs/servers.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 # source utils/defs/hostnames.sh
 
 # server_manager.sh - Main server management functions
@@ -15,28 +16,30 @@ Server_Start() {
     local SESSIONNAME="$4"
     local WEEKNUM=$5
 
-    screen -S $SESSIONNAME -p $NAME -X stuff "cd /home/rampage/zandronum3.2\n"
+    screen -S ${SESSIONNAME} -p ${NAME} -X stuff "cd /home/rampage/zandronum3.2\n"
 
-    if [ $SESSIONNAME == "rampage" ]; then
-        echo "Starting Rampage Servers: $NAME | ID: $ID"
-        local hostname_string=$(build_regular_hostname $ID)
-        SERVERSTRING="./zandronum-server -port $PORT -file ${wadlist[$ID]} ${configs[$ID]} ${hostname_string}"
-    elif [ $SESSIONNAME == "vengeance" ]; then
-        if [ $ID -lt 2 ]; then
-            echo "Starting Vengeance Game Servers: $NAME | ID: $ID"
-            local hostname_string=$(build_vengeance_hostname $WEEKNUM $ID)
-            SERVERSTRING="./zandronum-server -port $PORT -file ${veng_wadlist[1]} ${veng_configs[1]} ${hostname_string}"
+    if [ ${SESSIONNAME} == "rampage" ]; then
+        echo "Starting Rampage Servers: ${NAME} | ID: ${ID}"
+        local hostname_string=$(build_regular_hostname ${ID})
+        SERVERSTRING="./zandronum-server -port ${PORT} -file ${wadlist[$ID]} ${configs[$ID]} ${hostname_string}"
+    elif [ ${SESSIONNAME} == "vengeance" ]; then
+        if [ ${ID} -lt 2 ]; then
+            echo "Starting Vengeance Game Servers: ${NAME} | ID: ${ID}"
+            local veng_week_config="${VENG}/veng_${veng_season[${VENG_WEEKNUM}]}.cfg"
+            local hostname_string=$(build_vengeance_hostname ${WEEKNUM} ${ID})
+            SERVERSTRING="./zandronum-server -port ${PORT} -file ${veng_wadlist[1]} ${veng_configs[1]} ${hostname_string}"
         else
-            echo "Starting Vengeance Practice Servers: $NAME | ID: $ID"
-            local hostname_string=$(build_vengeance_hostname $WEEKNUM $ID)
-            SERVERSTRING="./zandronum-server -port $PORT -file ${veng_wadlist[0]} ${veng_configs[0]} ${hostname_string}"
+            echo "Starting Vengeance Practice Servers: ${NAME} | ID: ${ID}"
+            local veng_week_config="${VENG}/veng_${veng_season[$VENG_WEEKNUM]}.cfg"
+            local hostname_string=$(build_vengeance_hostname ${WEEKNUM} ${ID})
+            SERVERSTRING="./zandronum-server -port ${PORT} -file ${veng_wadlist[0]} ${veng_configs[0]} ${hostname_string}"
         fi
     else
-        echo "Starting Servers: $NAME | ID: $ID"
+        echo "Starting Servers: ${NAME} | ID: ${ID}"
     fi
 
-    echo "Starting Server $NAME | ID: $ID ..."
-    echo "Server String is $SERVERSTRING"
+    echo "Starting Server ${NAME} | ID: ${ID} ..."
+    echo "Server String is ${SERVERSTRING}"
     screen -S ${SESSIONNAME} -p ${NAME} -X stuff "${SERVERSTRING}^M"
     sleep 2
     echo "..."
